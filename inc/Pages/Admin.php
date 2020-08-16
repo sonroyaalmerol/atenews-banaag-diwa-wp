ce578ef589808c83d063ee7a26ce1f0913dba35d
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-08-16 20:07:08 +0800

    show to admin and editoors only

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -10,6 +10,13 @@ namespace Inc\Pages;
 class Admin {
 	public function register() {
 		add_action('admin_menu', array($this, 'add_admin_pages'));
+		add_action('admin_menu', function() {
+			if(!current_user_can('editor') && !current_user_can('administrator')) {
+				remove_menu_page('edit.php?post_type=photo_essay_sub');
+				remove_menu_page('edit.php?post_type=poem_sub');
+				remove_menu_page('edit.php?post_type=short_story_sub');
+			}
+		});
 		add_action('admin_init', function() {
 			register_setting('banaag-diwa-submissions-settings', 'banaag_diwa_submissions_state', array('default' => 'open'));
 			register_setting('banaag-diwa-submissions-settings', 'banaag_diwa_submissions_delete', array('default' => 'reject'));
